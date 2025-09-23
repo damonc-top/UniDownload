8d638e12282dbcbe1b704635f888bf96073333e1
afeng <damonc@126.com>
2025-09-23 22:45:16 +0800

    下载测试代码还没有想好，稍等

--- UniDownloadTest/TestHelper.cs
+++ UniDownloadTest/TestHelper.cs
@@ -2,6 +2,9 @@ namespace UniDownload.UniDownloadTest
 {
     public class TestHelper
     {
-        
+        public static void TestLoad()
+		{
+			// DownloadManager mgr = new DownloadManager(); 
+		}
     }
 }
